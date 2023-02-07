e99288a056e9b8531c19d303a715e37221aad3c6
Conner <65339198+connercsbn@users.noreply.github.com>
2023-02-06 20:50:50 -0800

    add to profile menu

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -12,7 +12,7 @@ const links = [
 
 export default function Nav() {
   const { data: sessionData } = useSession();
-  const [navOpen, setNavOpen] = useState(false);
+  const [navOpen, setNavOpen] = useState(true);
   const router = useRouter();
   const activeClasses =
     "mx-1 relative inline-flex items-center rounded-md border border-transparent px-2 py-1 transition-colors hover:no-underline border bg-purple-200/50 text-purple-800 dark:border-purple-200/20 dark:bg-purple-200/10 dark:hover:border-purple-200/50 rounded-lg text-purple-800 hover:bg-purple-200/50 hover:text-purple-800 dark:text-purple-100 dark:hover:bg-purple-200/10 dark:hover:text-purple-300";
@@ -56,14 +56,29 @@ export default function Nav() {
                     setNavOpen(false);
                   }}
                 >
-                  <div className="absolute right-0 z-10">
-                    <div className=" divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700">
-                      <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
-                        <li>
+                  <div className="absolute right-0 z-10 w-56">
+                    <div className="divide-y divide-gray-100 rounded-lg bg-slate-900 p-4 shadow">
+                      <ul className="text-sm text-gray-700 dark:text-gray-200">
+                        <li className="mb-2 flex items-center">
+                          <Image
+                            className="mr-3 aspect-square h-full rounded-full"
+                            src={sessionData.user?.image as string}
+                            alt=""
+                            width={40}
+                            height={40}
+                          ></Image>
+                          {sessionData.user?.name}
+                        </li>
+                        <div
+                          className="block h-[1px] w-full bg-white/20"
+                          role="separator"
+                        />
+                        <li className="mt-2">
                           <button
-                            className="block p-2  hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                            className="flex w-full items-center rounded-md p-1  hover:bg-white/10"
                             onClick={() => void signOut()}
                           >
+                            <SignOutSVG />
                             Sign out
                           </button>
                         </li>
@@ -86,3 +101,21 @@ export default function Nav() {
     </>
   );
 }
+
+const SignOutSVG = () => (
+  <svg
+    width="17"
+    height="17"
+    version="1.1"
+    viewBox="0 0 20 20"
+    x="0px"
+    y="0px"
+    fill="white"
+    className="mr-2"
+  >
+    <g>
+      <path d="M16 18h-4a2 2 0 01-2-2v-2h2v2h4V4h-4v2h-2V4a2 2 0 012-2h4a2 2 0 012 2v12a2 2 0 01-2 2z"></path>
+      <path d="M7 5l1.5 1.5L6 9h8v2H6l2.5 2.5L7 15l-5-5 5-5z"></path>
+    </g>
+  </svg>
+);
