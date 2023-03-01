b2212b155e9d65b7a06239c790602b178868262e
Conner <65339198+connercsbn@users.noreply.github.com>
2023-03-01 15:32:48 -0800

    move morebutton and make checkbox bigger

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -107,40 +107,4 @@ const MyButton = ({
   );
 };
 
-const MoreButton = ({
-  hovering,
-}: PropsWithChildren<{ hovering?: boolean }>) => {
-  return (
-    <>
-      <button
-        data-tooltip-id="more-button"
-        data-tooltip-content="edit stuff about streamer"
-        data-tooltip-delay-show={200}
-        className={
-          "relative z-10 ml-1 h-6 w-5 rounded-md text-gray-300 transition hover:bg-white/10 hover:text-white"
-        }
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="relative right-[2px] h-6 w-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
-          />
-        </svg>
-      </button>
-      {hovering &&
-        createPortal(
-          <Tooltip className="z-50" id="more-button" place="right" />,
-          document.body
-        )}
-    </>
-  );
-};
-
-export { PlusButton, MinusButton, X, MyButton, MoreButton };
+export { PlusButton, MinusButton, X, MyButton };
--- src/components/streamerInList.tsx
+++ src/components/streamerInList.tsx
@@ -1,11 +1,13 @@
 import { type PropsWithChildren } from "react";
 import Image from "next/image";
-import { MoreButton } from "./buttons";
+import { createPortal } from "react-dom";
 import type { Streamer } from "@prisma/client";
 import { useState } from "react";
 import { MyButton, MinusButton } from "./buttons";
 import Modal from "./modal";
 import { type RouterOutputs } from "../utils/api";
+import { Menu } from "@headlessui/react";
+import { Tooltip } from "react-tooltip";
 
 export default function Streamer({
   streamer,
@@ -87,7 +89,7 @@ export default function Streamer({
               checked={streamer.isOnCalendar}
               type="checkbox"
               onChange={() => handleToggleCalendar(streamer.id)}
-              className="ml-2 h-7"
+              className="ml-2 h-7 w-4"
             />
           )}
         </div>
@@ -111,3 +113,71 @@ const PlusButton = () => (
     />
   </svg>
 );
+
+const MoreButton = ({
+  hovering,
+}: PropsWithChildren<{ hovering?: boolean }>) => {
+  return (
+    <>
+      <button
+        data-tooltip-id="more-button"
+        data-tooltip-content="edit stuff about streamer"
+        data-tooltip-delay-show={200}
+        className={
+          "relative z-10 ml-1 h-6 w-5 rounded-md text-gray-300 transition hover:bg-white/10 hover:text-white"
+        }
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="relative right-[2px] h-6 w-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+          />
+        </svg>
+      </button>
+      {hovering &&
+        createPortal(
+          <Tooltip className="z-50" id="more-button" place="right" />,
+          document.body
+        )}
+    </>
+  );
+};
+function MyDropdown() {
+  return (
+    <Menu>
+      <Menu.Button>More</Menu.Button>
+      <Menu.Items>
+        <Menu.Item>
+          {({ active }) => (
+            <a
+              className={`${(active && "bg-blue-500") || ""}`}
+              href="/account-settings"
+            >
+              Account settings
+            </a>
+          )}
+        </Menu.Item>
+        <Menu.Item>
+          {({ active }) => (
+            <a
+              className={`${(active && "bg-blue-500") || ""}`}
+              href="/account-settings"
+            >
+              Documentation
+            </a>
+          )}
+        </Menu.Item>
+        <Menu.Item disabled>
+          <span className="opacity-75">Invite a friend (coming soon!)</span>
+        </Menu.Item>
+      </Menu.Items>
+    </Menu>
+  );
+}
